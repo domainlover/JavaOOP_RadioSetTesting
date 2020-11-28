b2b2fe0c2277f0adc68653e9945e4de4e7dc7fa5
Dmitry Urin <domainlover@yahoo.com>
2020-11-28 13:41:39 +0200

    Changes added

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -16,26 +16,6 @@ public class Radio {
     private int minSoundVolume;
     private int maxSoundVolume;
 
-    public void setCurrentRadioStation(int currentRadioStation) {
-        if (currentRadioStation > maxRadioStation) {
-            return;
-        }
-        if (currentRadioStation < minRadioStation){
-            return;
-        }
-        this.currentRadioStation = currentRadioStation;
-    }
-
-    public void setCurrentSoundVolume(int currentSoundVolume) {
-        if (currentSoundVolume >= maxSoundVolume){
-            this.currentSoundVolume = maxSoundVolume;
-            return;
-        }
-        if (currentSoundVolume <= minSoundVolume){
-            return;
-        }
-        this.currentSoundVolume = currentSoundVolume;
-    }
 
     public int getCurrentRadioStation() {
         return currentRadioStation;
